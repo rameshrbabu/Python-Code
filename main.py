33f82a74a8bd4996719d30add2511450a46c4950
rameshrbabu <rameshrbabu2@gmail.com>
2026-02-04 20:08:29 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+Name="Rithwik"
+# print ("My name is", Name)
+
+# comments
+
+"""comment1
+comment2
+comment3
+ """
+
+school9= "eighth grade"
+number_= 176
+State="New Jersey"
+def Myfuncion():
+    print(school9)
+    print(number_)
+    print(State)
+Myfuncion()
+
+# schoolname=input("Enter Your school name")
+# print(schoolname)
+
+# Statename=input("Enter your state name")
+# print("My state name is",Statename)
+# age=input("How old are you?")
+# print(age)
+# bool
+#float
+areaOfCircle = 2.10
+print(type(areaOfCircle))
+
+# bool
+isNumber = False
+print(type(isNumber))
+
+number = 200
+print(type(number))
+string = "hello"
+print(type(string))
+print(type(number))
+deci = 6.7
+print(type(deci))
+
+# type conversion
+a=10
+print(type(10))
+# This is the place where we type
+#conversion begins
+b=float(a)
+print(type(b))
+x="10"
+print(type(x))
+y=(int(x))
+print(type(y))
+#float
+x1=10.2
+print(type(x1))
+y1=(int(x1))
+print(type(y1))
+name=input("What's your name? ")
+print("My name is "+name)
+
+schoolname=input("What's your school name? ")
+print("My school name is "+schoolname)
