cf693576c0179c99d840cdef91202daa85906c06
rameshrbabu <rameshrbabu2@gmail.com>
2026-02-11 20:24:52 -0500

    Add files via upload

---
+++ type-conversion.py
@@ -0,0 +1,66 @@
+# Date types
+
+# Integer - Whole Number
+# Float - Decimal Numbers
+# String - text (with in a quotes)
+# Boolean - True(yes/1) or False(No/0)
+
+floatNum = 10.20
+
+# type is used to know the type of the variable
+# print(type(name))
+# print(type(age))
+# print(type(floatNum))
+
+
+# str
+name = "John"
+print(type(name))
+
+# int
+luckyNum = 10
+print(type(luckyNum))
+
+# bool
+float
+areaOfCircle = 2.10
+print(type(areaOfCircle))
+
+# bool
+isNumber = False
+print(type(isNumber))
+
+# type conversion
+a = 10
+print(type(a))
+# this is the place where type conversion begins
+b = float(a)
+print(type(b))
+
+x = "10"
+print(type(x))
+y = int(x)
+print(type(y))
+
+# float
+x1 = 10.2
+print(type(x1))
+y1 = int(x1)
+print(type(y1))
+
+# name = input("What is your name?")
+# print("My name is "+name)
+
+a2 = 10
+b2 = 10
+print(a2+b2)
+
+
+a1 = int(input("Enter num1 "))
+b1 = int(input("Enter num2 "))
+print("Sum of num1 and num2 is ", a1+b1)
+d= input("Enter num1 ")
+c = input("Enter num2 ")
+print("Sum of num1 and num2 is ", d+c)
+# input -> is used to getting value from the user
+# whatever the value we are getting from the user it will be considered as string that is wh 
